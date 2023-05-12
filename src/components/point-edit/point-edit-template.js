d3214af982e1532b88074d5cf8c14b9f006df7be
Alexey <avorobyev174@gmail.com>
2023-05-12 11:30:57 +0500

    Правки

--- src/components/point-edit/point-edit-template.js
+++ src/components/point-edit/point-edit-template.js
@@ -55,12 +55,6 @@ function createEditPointTemplate({ type, destanation, dateFrom, dateTo, price, o
           <label class="event__label  event__type-output" for="event-destination-1">
             ${ typeTitle }
           </label>
-          <!--<input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${ name }" list="destination-list-1">
-          <datalist id="destination-list-1">
-            <option value="Amsterdam"></option>
-            <option value="Geneva"></option>
-            <option value="Chamonix"></option>
-          </datalist>-->
           <select class="event__input  event__select--destination" id="event-destination-1" type="text" name="event-destination">
             ${ createPointDestanationOptionsTemplate(name) }
           </select>
